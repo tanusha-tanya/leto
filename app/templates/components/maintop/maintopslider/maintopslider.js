f99c5a2b2f8e669dcd6f53fe02026cd8c199bbdf
Verevkina T <verevkina@picom.ru>
2018-11-14 17:56:04 +0400

    slider

---
+++ app/templates/components/maintop/maintopslider/maintopslider.js
@@ -0,0 +1,10 @@
+$('.maintopslider').owlCarousel({
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    loop:true,
+    items:1,
+    smartSpeed:450
+});
