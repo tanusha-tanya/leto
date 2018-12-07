3d3d225216cb910f31ab73a278133fa70cf03d16
Verevkina T <verevkina@picom.ru>
2018-12-07 19:06:14 +0400

    menu

--- app/templates/components/maintop/maintopmenu/maintopmenu.js
+++ app/templates/components/maintop/maintopmenu/maintopmenu.js
@@ -1,10 +1,27 @@
 const sub = document.querySelectorAll(".maintopmenu-link__sub");
+const submenu = document.querySelector(".maintopmenu-submenu");
 if(sub){
-  //const submenu = document.querySelector(".maintopmenu-submenu");
   sub.forEach((elem)=>{
     elem.addEventListener('click',(e)=>{
       e.preventDefault();
-      elem.parentNode.querySelector('.maintopmenu-submenu').classList.toggle('active')
+      sub.forEach((el)=>{
+        console.log(el == elem);
+        if(el == elem ){
+          if(el.classList.contains('active')){
+            el.classList.remove('active');
+            submenu.classList.remove('active');
+            submenu.innerHTML = "";
+          }
+          else{
+            el.classList.add('active');
+            submenu.classList.add('active');
+            submenu.innerHTML = el.parentNode.querySelector('.maintopmenu-inner').innerHTML;
+          }
+        }
+        else{
+          el.classList.remove('active');
+        }
+      });
     })
   })
 }
