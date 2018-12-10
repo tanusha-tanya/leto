bea0e6736da3f9657570528d41d4a39bc6c135f2
Verevkina T <verevkina@picom.ru>
2018-12-10 17:33:39 +0400

    breadcrumbs

--- app/templates/components/header/headericons/headericons.js
+++ app/templates/components/header/headericons/headericons.js
@@ -1,11 +1,16 @@
-const search = document.querySelector(".js-search")
+const search = document.querySelector('.js-search');
+const bodydark = document.querySelector('.body-dark');
 if(search){
-  const searchmenu = document.querySelector(".mobilesearch");
-  const close = document.querySelector(".mobilesearch-close");
+  const searchmenu = document.querySelector('.mobilesearch');
+  const close = document.querySelector('.mobilesearch-close');
   search.onclick = function(){
-    searchmenu.classList.add("open")
+    searchmenu.classList.add('open');
+    console.log(bodydark);
+    bodydark.classList.add('active');
   }
   close.onclick = function(){
-    searchmenu.classList.remove("open")
+    searchmenu.classList.remove('open');
+    console.log(bodydark);
+    bodydark.classList.remove('active');
   }
 }
--- app/templates/components/maintop/maintopmenu/maintopmenu.js
+++ app/templates/components/maintop/maintopmenu/maintopmenu.js
@@ -1,21 +1,21 @@
-const sub = document.querySelectorAll(".maintopmenu-link__sub");
-const submenu = document.querySelector(".maintopmenu-submenu");
+const sub = document.querySelectorAll('.maintopmenu-link__sub');
+const submenu = document.querySelector('.maintopmenu-submenu');
 if(sub){
   sub.forEach((elem)=>{
     elem.addEventListener('click',(e)=>{
       e.preventDefault();
       sub.forEach((el)=>{
-        console.log(el == elem);
         if(el == elem ){
           if(el.classList.contains('active')){
             el.classList.remove('active');
             submenu.classList.remove('active');
-            submenu.innerHTML = "";
+            submenu.innerHTML = '';
           }
           else{
             el.classList.add('active');
             submenu.classList.add('active');
             submenu.innerHTML = el.parentNode.querySelector('.maintopmenu-inner').innerHTML;
+            missClick(el)
           }
         }
         else{
@@ -24,4 +24,15 @@ if(sub){
       });
     })
   })
+}
+function missClick(el){
+  document.body.onclick = function(e){    
+    if(e.target.closest('.maintopmenu-submenu') == null && e.target != submenu && e.target != el){
+      submenu.classList.remove('active');
+      submenu.innerHTML = '';
+      sub.forEach((elem)=>{
+        elem.classList.remove('active');
+      })
+    }
+  }
 }
