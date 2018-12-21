e7278d1194ead9ee76699697f784766f4328f286
Verevkina T <verevkina@picom.ru>
2018-12-21 18:31:54 +0400

    pixelperfect

--- app/templates/components/header/headersearch/search.js
+++ app/templates/components/header/headersearch/search.js
@@ -5,32 +5,38 @@ if(search){
   let list = document.querySelector('.autocomplete');
   let li = "";
   let array = [];
+  let strings = [];
   let flag = true;
+
   function parseJson(word){
     fetch(json)
+
   	.then(function (response) {
   		if (response.status !== 200) {
   		  return Promise.reject(new Error(response.statusText))
   		}
   		return Promise.resolve(response)
   	})
+
   	.then(function (response) {
   		return response.json()
   	})
+
   	.then(function (data) {
       data.forEach((string)=>{
         let smallString = string.item.toLowerCase().trim();
         if(smallString.indexOf(word) != -1){
-          setArray(string);
+          checkArray(string)
+          showAutocomplete()
         }
         else{
-          hideAutocomplite(array)
+          hideAutocomplete()
         }
       })
   	})
 
   	.catch(function (error) {
-  		document.body.innerHTML = error;
+  		console.log(error);
   	})
   }
 
@@ -40,47 +46,36 @@ if(search){
       parseJson(word);
     }
     else{
-      hideAutocomplite(array)
+      hideAutocomplete(strings)
     }
   }
 
-  function setArray(string){
-    if (array.length > 0){
-      checkArray(string.item);
-      if (flag === true){
-        array.push(string);
-      }
-      else{
-        return
-      }
-    }
-    else {
+  function checkArray(string){
+    if(array.length === 0){
       array.push(string);
+      strings.push(string.item);
     }
-    showAutocomplite(array)
-  }
-
-  function checkArray(string, flag){
-    let newArr = array.filter(function(words) {
-      return string.indexOf(words.item) != -1;
-    });
-    if(newArr.length > 0){
-      flag = false;
+    else{
+      if(strings.indexOf(string.item) == -1){
+        array.push(string);
+        strings.push(string.item);
+      }
     }
   }
 
-  function showAutocomplite(array){
+  function showAutocomplete(){
     list.classList.add("active");
+    list.innerHTML = "";
     array.forEach((item) =>{
       li += `<li><a href="${item.link}">${item.item}</a></li>`
     });
     list.innerHTML = li;
   }
 
-  function hideAutocomplite(array){
+  function hideAutocomplete(){
     list.classList.remove("active");
-    li = "";
-    list.innerHTML = li;
+    list.innerHTML = "";
     array = [];
+    strings = []
   }
 }
