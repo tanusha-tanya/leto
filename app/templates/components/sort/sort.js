f907f5c9ace0c4a7d09f1ee7d6f8c446a236825f
Verevkina T <verevkina@picom.ru>
2019-01-18 16:12:04 +0400

    salon tablet

--- app/templates/components/sort/sort.js
+++ app/templates/components/sort/sort.js
@@ -6,11 +6,19 @@ if(sort){
   })
   const sortLink = document.querySelectorAll('.sort-link')
   sortLink.forEach((elem)=>{
-    elem.addEventListener('click',()=>{
-        for(let i = 0; i < sortLink.length; i++){
-          sortLink.classList.remove('.sort-link__active')
+    elem.addEventListener('click',(e)=>{
+      e.preventDefault()
+      if(!elem.classList.contains('sort-link__active')){
+        sortType.textContent = elem.textContent
+        elem.classList.add('sort-link__active')
+      }
+      for(let i = 0; i < sortLink.length; i++){
+        if(sortLink[i] !== elem){
+          sortLink[i].classList.remove('sort-link__active')
         }
-        elem.classList.toggle('.sort-link__active')
+      }
+      sort.classList.remove('sort-block__active')
     })
   })
+
 }
