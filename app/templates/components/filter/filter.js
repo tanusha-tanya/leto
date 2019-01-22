2f07fa06aca23a3c3a22e06092b4ae4d39184ac6
Verevkina T <verevkina@picom.ru>
2019-01-22 17:03:52 +0400

    card

---
+++ app/templates/components/filter/filter.js
@@ -0,0 +1,7 @@
+const filter = document.querySelector('.filter');
+
+if(filter){
+  filter.addEventListener('click',()=>{
+    deskfilter.classList.add('deskfilter__active')
+  })
+}
--- app/templates/components/filter_desktop/filter_desktop.js
+++ app/templates/components/filter_desktop/filter_desktop.js
@@ -16,5 +16,33 @@ if (deskfilter){
       }
     })
   })
+
+  const brandCheck = document.querySelectorAll('.deskfilter-scroll-checkbox')
+  const counter = document.querySelector('.counter');
+  const reset = document.querySelector('.deskfilter-reset');
+
+  brandCheck.forEach((elem)=>{
+    elem.addEventListener('change',()=>{
+      let j = 0;
+      for(let i = 0; i < brandCheck.length; i++){
+        if(brandCheck[i].checked){
+          j++;
+        }
+      }
+      counter.textContent = j;
+    })
+  })
+
+  reset.addEventListener('click', (e)=>{
+    e.preventDefault();
+    brandCheck.forEach((elem)=>{
+      elem.checked = false;
+      counter.textContent = 0;
+    })
+  })
+  const close = document.querySelector('.deskfilter-close');
+   close.addEventListener('click', (e)=>{
+     deskfilter.classList.remove('deskfilter__active')
+   })
 }
 $('.deskfilter-scroll-container').mCustomScrollbar();
