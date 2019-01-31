15a3866ef12c6fa14350a418da5b2362aeb5925b
Tatyana Verevkina <verevkina@picom.ru>
2019-01-31 11:58:59 +0400

    cartgoods

--- app/templates/components/satellite/satellite.js
+++ app/templates/components/satellite/satellite.js
@@ -1,8 +1,18 @@
 $('.cartgoods-satellite-slider').owlCarousel({
-    items:1,
     center: true,
     arrows: false,
-    dots: true
+    dots: true,
+    responsive : {
+       0: {
+        items:1,
+        center: true
+      },
+       768: {
+        items:2,
+        center: false,
+        margin: 20
+      }
+    }
 });
 
     
