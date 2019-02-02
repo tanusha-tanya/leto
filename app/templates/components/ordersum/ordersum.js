c74ed80df6b027aee6e45f6e73f5472998a36742
Tatyana Verevkina <verevkina@picom.ru>
2019-02-02 11:34:15 +0400

    order

--- app/templates/components/ordersum/ordersum.js
+++ app/templates/components/ordersum/ordersum.js
@@ -1,7 +1,10 @@
 const orderOpener = document.querySelector('.order-sum-openspoiler')
 if (orderOpener){
     let orderList = document.querySelector('.order-sum-spoiler')
+    let linkText = document.querySelector('.order-sum-link')
     orderOpener.onclick = function(){
+        orderOpener.classList.toggle('order-sum-openspoiler__active')
         orderList.classList.toggle('order-sum-spoiler__active')
+        linkText.textContent=="Свернуть" ? linkText.textContent="Посмотреть весь заказ" : linkText.textContent="Свернуть"
     }
 }
