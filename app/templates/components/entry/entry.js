16c00ded95d7b37d79603b9c33199e8bf4404b2c
Tatyana Verevkina <verevkina@picom.ru>
2019-02-19 18:10:02 +0400

    password

--- app/templates/components/entry/entry.js
+++ app/templates/components/entry/entry.js
@@ -1,20 +1,60 @@
+let hide = true;
+document.body.onclick = (e)=>{
+    if(e.target.classList.contains('js-button')){
+       e.preventDefault();
+       let form = e.target.closest('.js-form');
+       check(form);
+    }
+    else if(e.target.classList.contains('.js-password')){
+        let svgPath = e.target.querySelector('use');
+        let svgShow = svgPath.dataset.href;
+        let password = svg.parrentNode.querySelector('input');
+        hide = !hide;
+
+    }
+}
+let check = function(form){
+    let inputCollection = form.querySelectorAll('.js-inputcheck')
+    let errorText = form.querySelector('.error-text')
+    let name = null;
+    let message = null;
+    let array = [];
+    inputCollection.forEach((input)=>{
+        if(input.value.length==0){
+            input.classList.add('error');
+            name = input.name;
+            array.push(name)            
+        }
+        else{
+            input.classList.remove('error');
+        }
+    })
+    if(array.length > 0){
+        if(array.length === 1){
+            message = "Не заполнено поле" + array[0];
+        }
+        else{
+            message = "Не заполнены поля";
+            for(let i=0; i < array.length; i++){
+                message += ' ' + array[i];
+            }
+        } 
+        errorText.textContent = message;
+    }
+    else{
+         errorText.textContent = '';         
+    }
+}
+
+document.body.onchange = (e)=>{
+    if(e.target.classList.contains('js-inputcheck')){
+       let form = e.target.closest('.js-form');
+       check(form)
+    }
+}
 const formCollection = document.querySelectorAll('.js-form')
 if(formCollection){
     formCollection.forEach((form) => {
-        consolr.log(form)
+        console.log(form)
     })
-}
-
-const passwordCollection = document.querySelectorAll('.js-password');
-if(passwordCollection){
-    let svg = null;
-    let input = null;
-    passwordCollection.forEach((password)=>{
-        svg = password.querySelector('.entry-eye');
-        input = password.querySelector('.entry-input');
-        svg.click = changeVisible(svg, input);
-    })
-    let changeVisible = (svg, input) => {
-        alert(svg)
-    }
 }
