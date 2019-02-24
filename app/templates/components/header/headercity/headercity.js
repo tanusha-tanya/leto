7fad22f1f04203cb53371141cb56247403eee501
Tatyana Verevkina <verevkina@picom.ru>
2019-02-24 17:54:32 +0400

    headercity

--- app/templates/components/header/headercity/headercity.js
+++ app/templates/components/header/headercity/headercity.js
@@ -1,17 +1,33 @@
-const citiesPopupLink = document.querySelector('.js-cities');
-if(citiesPopupLink){    
-    let citiesPopup = document.querySelector('.cities')
-    let close = citiesPopup.querySelector('.cities-close')
-    citiesPopupLink.onclick = (e) => {
-        e.preventDefault();
-        citiesPopup.classList.toggle('active')
+const citiesPopupLink = document.querySelectorAll('.js-cities'),
+citiesPopup = document.querySelector('.cities'),
+close = citiesPopup.querySelector('.cities-close'),
+headercityCollection = document.querySelectorAll('.headercity')
+
+if(citiesPopupLink){  
+    citiesPopupLink.forEach((link) =>{
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            if(!link.closest('.headercity').querySelector('.cities')){
+                link.closest('.headercity').append(citiesPopup);
+                citiesPopup.classList.add('active');
+            }
+            else{
+                citiesPopup.classList.remove('active');
+                 link.closest('.headercity').removeChild(citiesPopup);
+            }
+        })           
+    })
+     close.onclick = () => {
+        citiesPopup.classList.remove('active');
+        this.closest('.headercity').removeChild(citiesPopup);
     }
     document.body.onclick = (e) => {
-        if(e.target !== citiesPopup && !e.target.closest('.cities') && e.target !== citiesPopupLink){
-            citiesPopup.classList.remove('active')
+        if(e.target !== citiesPopup && !e.target.closest('.cities') && !e.target.classList.contains('js-cities')){
+           citiesPopup.classList.remove('active')  
+           headercityCollection.forEach((headercity) => {
+               headercity.removeChild(citiesPopup);
+           })
         }
     }
-    close.onclick = () => {
-        citiesPopup.classList.remove('active')
-    }
+    
 }
