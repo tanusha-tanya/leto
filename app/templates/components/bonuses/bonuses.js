f7e18315da92ddb28ea3870163c642321def2777
tanusha-tanya <offchinnikova@bk.ru>
2019-08-19 18:11:49 +0400

    bonuses

--- app/templates/components/bonuses/bonuses.js
+++ app/templates/components/bonuses/bonuses.js
@@ -2,9 +2,10 @@ let copyPromo = document.querySelectorAll('.bonuses-copy')
 if(copyPromo){
     let number = null;
     copyPromo.forEach((copy) =>{
-        copy.addEventListener('click', ()=>{
-            number = copy.parentNode.querySelector('.bonuses-number');
-            number.select();
+        copy.addEventListener('click', (e)=>{
+          e.preventDefault();
+          number = copy.parentNode.querySelector('.bonuses-number');
+          number.select();
   try {  
     var successful = document.execCommand('copy');  
     var msg = successful ? 'successful' : 'unsuccessful';   
