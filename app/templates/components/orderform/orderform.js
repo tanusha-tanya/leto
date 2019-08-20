44896b4b59fdb924b0e2b4e735e33a6bcb9febce
tanusha-tanya <offchinnikova@bk.ru>
2019-08-20 19:13:27 +0400

    orderform

--- app/templates/components/orderform/orderform.js
+++ app/templates/components/orderform/orderform.js
@@ -3,3 +3,16 @@ $('body').on('click','.js-close', function(event){
     let input = this.parentNode.querySelector("input");
         input.value = "";
 })
+let choiceCheckbox = document.querySelector('#choice')
+let addresslist = document.querySelector('.order-addreslist')
+$('.order-addreslist').mCustomScrollbar();
+if(choiceCheckbox){
+    choiceCheckbox.addEventListener('change', ()=>{
+        if (choiceCheckbox.checked){
+            addresslist.classList.add('open')
+        }
+        else{
+            addresslist.classList.remove('open')
+        }
+    })
+ }
