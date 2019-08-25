40d3984dc6d1abe0796e7b983f70e7e5f01c4f10
Tatyana Verevkina <verevkina@picom.ru>
2019-08-25 21:32:49 +0400

    delivery

--- app/templates/components/delivery/delivery.js
+++ app/templates/components/delivery/delivery.js
@@ -1,9 +1,9 @@
-const radioCollection = document.querySelectorAll('.delivery-radio');
-if(radioCollection){
+const radioCollectionDelivery = document.querySelectorAll('.delivery-radio');
+if(radioCollectionDelivery){
     let dataTarget = null;
     let dataFor = null;
     let methodCollection = document.querySelectorAll('.delivery-method-item')
-    radioCollection.forEach((radio) => { 
+    radioCollectionDelivery.forEach((radio) => { 
         radio.addEventListener('click', () =>{            
             dataTarget = radio.dataset['target'];
             methodCollection.forEach((method) => {
@@ -16,8 +16,9 @@ if(radioCollection){
         })   
     })
 }
-
-$(function(){
+const map = document.querySelector('#map')
+if(map){
+    $(function(){
     ymaps.ready(init);    
     function init() {
     $('#pickup').click(function(){
@@ -68,8 +69,10 @@ $(function(){
 }
 $('.delivery-addresses-items').mCustomScrollbar();
 })
+}
+
 const mapLinks = document.querySelectorAll('.delivery-addresses-item');
-if(mapLinks){
+if(mapLinks.length >= 2){
     mapLinks.forEach((mapLink)=>{
         mapLink.addEventListener('click', ()=>{
             mapLinks.forEach((elem)=>{
@@ -79,4 +82,72 @@ if(mapLinks){
         })
     })
     
+}
+
+let cityDelivery = document.querySelector('.delivery-city')
+if(cityDelivery){
+    let cityInputDelivery = document.querySelector('.delivery-city-input');
+    let hiddenCityDelivery = document.querySelector("#js-ordercityhide");
+    let cityCompleet = document.querySelector('#js-citycompleet');
+   
+    cityInputDelivery.addEventListener('input', () => { 
+        cityCompleet.innerHTML = "";
+        cityCompleet.classList.remove('active');        
+        if(cityInputDelivery.value.length > 0){ 
+            cityCompleet.innerHTML = "";
+            let string = cityInputDelivery.value.trim(); 
+            let path = `/ajax/location_get_cities/?city=${string}`           
+            fetch(path)
+            .then(function(response){
+              return response.json();
+            })
+            .then(function(myJson){     
+                if(myJson.length > 0){
+                  autocompleet(myJson) 
+                }   
+            });           
+            let autocompleet = (myJson) => {                
+                let ul = document.createElement('ul');
+                    ul.className = 'compleet-ul';   
+                for(let i = 0; i<myJson.length; i++){                    
+                    let li = document.createElement('li');
+                    li.className = 'compleet-item';
+                    li.innerHTML = `<a href="#" data-id="${myJson[i].ID}" class="compleet-link"><strong>${myJson[i].NAME}</strong> ${myJson[i].PARENT.NAME}</a>`
+                    ul.appendChild(li);                                     
+                }
+                cityCompleet.classList.add('active');
+                cityCompleet.appendChild(ul);
+                choiceCity();  
+                bodyClick();
+            } 
+        }
+        else{
+            cityCompleet.innerHTML = "";
+            cityCompleet.classList.remove('active');
+            cityInputDelivery.value = "";
+            hiddenCityDelivery.value = "";            
+        }
+
+        let bodyClick = () => {
+            document.body.addEventListener('click', (e) => {            
+                if(e.target !== cityCompleet && !e.target.closest('#js-citycompleet')){
+                    cityCompleet.innerHTML = "";
+                    cityCompleet.classList.remove('active');         
+                }
+            })
+        } 
+    })
+
+    let choiceCity = () => {
+        let compleetlinks = document.querySelectorAll('.compleet-link');        
+        compleetlinks.forEach((link)=>{
+            link.addEventListener('click', (e) =>{                
+                e.preventDefault();                            
+                cityInputDelivery.value = link.textContent;
+                hiddenCityDelivery.value = link.dataset.id;
+                cityCompleet.classList.remove('active');
+                cityCompleet.innerHTML = "";            
+            })
+        })
+    }
 }
