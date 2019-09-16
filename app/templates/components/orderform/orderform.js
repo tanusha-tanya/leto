cdb9351b21b8bb059ae65806f407a2e9dbd61b9c
tanusha-tanya <offchinnikova@bk.ru>
2019-09-16 16:06:58 +0400

    orderform

--- app/templates/components/orderform/orderform.js
+++ app/templates/components/orderform/orderform.js
@@ -6,7 +6,8 @@ $('.order-addreslist').mCustomScrollbar({
 
 let orderform = document.querySelector(".order-orderform")
 if(orderform){
-    let checkform = (form) =>{    
+    let orderForm = document.querySelector('.order-form')
+    let checkform = (form) =>{               
         let inputForm = form.querySelectorAll('.required-input');
         let buttonForm = form.querySelector('.send-button');
         let counter = 0;
@@ -22,20 +23,44 @@ if(orderform){
             buttonForm.classList.remove('disabled') 
         }
     }
-    checkform(orderform)
+    checkform(orderForm)
+
     let choiceCheckbox = document.querySelector('#choice')
     let addresslist = document.querySelector('.order-addreslist')
-    let orderInput = orderform.querySelectorAll('input') 
+    let orderInput = orderForm.querySelectorAll('input') 
+    let buttonForm = orderForm.querySelector('.order-button')  
+      
+    $('body').on('click','.order-button', function(event){        
+        let button = $(this);
+        if(button.hasClass('disabled')){
+            event.preventDefault();
+            return
+        }        
+    });
 
- 
     orderInput.forEach((input) => {
         input.addEventListener('change', ()=>{
-            checkform(orderform)
+            checkform(orderForm)
         });
-        input.addEventListener('input', ()=>{
-            checkform(orderform)
+        input.addEventListener('input', ()=>{           
+            checkform(orderForm)
         })
     })
+
+    $('.js-phone').on('input', () => {
+        checkform(orderForm)
+    })
+
+    let cityInput = document.querySelector("#js-ordercity");
+    let street = document.querySelector("#js-orderstreet");
+    let build = document.querySelector("#js-orderbuild");
+    let flat = document.querySelector("#js-orderflat");
+    let hiddenCity = document.querySelector("#js-ordercityhide");
+    let hiddenStreet = document.querySelector("#js-orderstreethide");
+    let radioCollection = document.querySelectorAll('.js-addresschoice');
+    let cityCompleet = document.querySelector('#js-citycompleet');
+    let cartAddress = document.querySelector('.js-cart-order-address');
+    let streetCompleet = document.querySelector('#js-streetcompleet'); 
 
     let hideAddress = () => {
         $(cartAddress).hide()
@@ -44,6 +69,14 @@ if(orderform){
         build.value = "";
         flat.value = "";
         checkform(orderform)
+    }
+
+    let clearAddress = () => {
+        cityCompleet.innerHTML = "";
+        cityCompleet.classList.remove('active');
+        hiddenStreet.value="";
+        hiddenCity.value = "";            
+        hideAddress();        
     }
 
     if(choiceCheckbox){  
@@ -55,22 +88,13 @@ if(orderform){
                 addresslist.classList.remove('open')
             }
         }) 
-    }
-    let cityInput = document.querySelector("#js-ordercity");
-    let street = document.querySelector("#js-orderstreet");
-    let build = document.querySelector("#js-orderbuild");
-    let flat = document.querySelector("#js-orderflat");
-    let hiddenCity = document.querySelector("#js-ordercityhide");
-    let hiddenStreet = document.querySelector("#js-orderstreethide");
-    let radioCollection = document.querySelectorAll('.js-addresschoice');
-    let cityCompleet = document.querySelector('#js-citycompleet');
-    let cartAddress = document.querySelector('.js-cart-order-address');
-    let streetCompleet = document.querySelector('#js-streetcompleet');    
+    }       
 
     let showAddress = () => {
         $(cartAddress).show()
     }
-    if(cityInput.value.length <= 0){
+
+    if(cityInput.value.length <= 0){        
         hideAddress();
     }
 
@@ -81,21 +105,20 @@ if(orderform){
                     showAddress(); 
                     let cityId = radio.dataset.city;    
                     let streetId = radio.dataset.street;
-                    let buildName =  radio.dataset.build;
-                    let flatName = radio.dataset.flat;                    
+                    let buildName = radio.dataset.build;
+                    let flatName = radio.dataset.flat;                                      
                     fetch(`/ajax/location_get_cities/?city_id=${cityId}`)                    
-                    .then(function(response){
+                    .then(function(response){                                            
                       return response.json();
                     })
-                    .then(function(myJson){     
+                    .then(function(myJson){  
                         if(myJson.length > 0){                            
                             let cityName = `${myJson[0].NAME} ${myJson[0].PARENT.NAME}`
                             cityInput.value = cityName;
-                            hiddenCity.value = cityId; 
+                            hiddenCity.value = cityId;
                         }   
-                    });  
-
-                    fetch(`/ajax/location_get_streets/?city_id=${cityId}&street_id=${streetId}`)
+                    });   
+                  fetch(`/ajax/location_get_streets/?city_id=${cityId}&street_id=${streetId}`)
                     .then(function(response){
                       return response.json();
                     })
@@ -104,14 +127,18 @@ if(orderform){
                             street.value = myJson[0].NAME;
                             hiddenStreet.value = myJson[0].ID;  
                         }   
-                    });  
+                    })
+                    .then(function(){
+                        checkform(orderForm); 
+                    });                    
                     build.value = buildName;                              
                     if(flatName){
                         flat.value = flatName;
                     }
                     else{
                         flat.value = ""; 
-                    }                   
+                    }  
+                    checkform(orderForm);                 
                 }
             })
         })
@@ -141,58 +168,75 @@ if(orderform){
     
     cityInput.addEventListener('keyup', () => {
         clearTimeout(typingTimer);
-        if (cityInput.value) {
+        if (cityInput.value !== '') {
             typingTimer = setTimeout(cityTyping, doneTypingInterval);
         }
-    });   
+    }); 
+    
+    cityInput.onkeydown = function() {
+        let key = event.keyCode || event.charCode;    
+        if( key == 8 || key == 46 ){            
+                clearAddress();      
+                checkform(orderForm)          
+        }           
+    };
     
     street.addEventListener('keyup', () => {
         clearTimeout(typingTimer);
-        if (street.value) {
+        if (street.value !== '') {            
             typingTimer = setTimeout(streetTyping, doneTypingInterval);
         }
     });   
     
+    street.onkeydown = function() {
+        let key = event.keyCode || event.charCode;    
+        if( key == 8 || key == 46 ){             
+            hiddenStreet.value="";     
+            checkform(orderForm)         
+        }           
+    };
+
     let cityTyping = () => {
         cityCompleet.innerHTML = "";
         cityCompleet.classList.remove('active');        
-        if(cityInput.value.length > 0){ 
-            cityCompleet.innerHTML = "";
-            let string = cityInput.value.trim(); 
-            let path = `/ajax/location_get_cities/?city=${string}`                     
-            fetch(path)
-            .then(function(response){
-              return response.json();
-            })
-            .then(function(myJson){     
-                if(myJson.length > 0){  
-                  autocompleet(myJson) 
-                }   
-            });           
-            let autocompleet = (myJson) => {                
-                let ul = document.createElement('ul');
-                    ul.className = 'compleet-ul';   
-                for(let i = 0; i<myJson.length; i++){                    
-                    let li = document.createElement('li');
-                    li.className = 'compleet-item';
-                    li.innerHTML = `<a href="#" data-id="${myJson[i].ID}" class="compleet-link"><strong>${myJson[i].NAME}</strong> ${myJson[i].PARENT.NAME}</a>`
-                    ul.appendChild(li);                                     
-                }
-                cityCompleet.classList.add('active');
-                cityCompleet.appendChild(ul);
-                choiceCity();  
-                bodyClick()
-            }
-        }
+        if(cityInput.value.length > 0){
+            cityFetch(cityInput.value)
+        }            
         else{
-            cityCompleet.innerHTML = "";
-            cityCompleet.classList.remove('active');
-            hiddenStreet.value="";
-            cityInput.value = "";
-            hiddenCity.value = "";            
-            hideAddress();
+            clearAddress();
         } 
+    }
     
+    let cityFetch = (word) => {
+        cityCompleet.innerHTML = "";
+        let string = word.trim(); 
+        let path = `/ajax/location_get_cities/?city=${string}`                     
+        fetch(path)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(myJson){     
+            if(myJson.length > 0){  
+                autocompleetCity(myJson) 
+            }   
+        });          
+     }
+
+        let autocompleetCity = (myJson) => {  
+            cityCompleet.innerHTML = "";              
+            let ul = document.createElement('ul');
+                ul.className = 'compleet-ul';   
+            for(let i = 0; i<myJson.length; i++){                    
+                let li = document.createElement('li');
+                li.className = 'compleet-item';
+                li.innerHTML = `<a href="#" data-id="${myJson[i].ID}" class="compleet-link"><strong>${myJson[i].NAME}</strong> ${myJson[i].PARENT.NAME}</a>`
+                ul.appendChild(li);                                     
+            }
+            cityCompleet.classList.add('active');
+            cityCompleet.appendChild(ul);
+            choiceCity();  
+            bodyClick()
+        }
 
         let choiceCity = () => {
             let compleetlinks = document.querySelectorAll('.compleet-link');        
@@ -208,18 +252,17 @@ if(orderform){
                     build.value = "";
                     flat.value = "";  
                     showAddress(); 
-                            
+                    checkform(orderForm)        
                 })
             })
-        } 
-    }
+        }     
     
     let streetTyping = () => { 
         streetCompleet.innerHTML = "";  
         if(street.value.length > 0){
             streetCompleet.innerHTML = ""; 
             let string = street.value.trim();            
-            let cityid = hiddenCity.value;
+            let cityid = hiddenCity.value;            
             let path = `/ajax/location_get_streets/?city_id=${cityid}&street=${string}`
             fetch(path)
             .then(function(response){
@@ -227,28 +270,31 @@ if(orderform){
             })
             .then(function(myJson){     
                 if(myJson.length > 0){
-                    autocompleet(myJson) 
+                    autocompleetStreet(myJson) 
                 }   
-            });                      
-            let autocompleet = (myJson) => {                
-                let ul = document.createElement('ul');
-                    ul.className = 'compleet-ul';   
-                for(let i = 0; i<myJson.length; i++){                    
-                    let li = document.createElement('li');
-                    li.className = 'compleet-item';
-                    li.innerHTML = `<a href="#" data-id="${myJson[i].ID}" class="compleet-link">${myJson[i].NAME}</a>`
-                    ul.appendChild(li);                                     
-                }
-                streetCompleet.classList.add('active');
-                streetCompleet.appendChild(ul);
-                choiceStreet();  
-                bodyClick();                         
-            }      
+            });                     
         }
         else{
             streetCompleet.classList.remove('active');
             streetCompleet.innerHTML = "";   
-        }    
+        }  
+    }     
+        let autocompleetStreet = (myJson) => {    
+            streetCompleet.innerHTML = "";             
+            let ul = document.createElement('ul');
+                ul.className = 'compleet-ul';   
+            for(let i = 0; i<myJson.length; i++){                    
+                let li = document.createElement('li');
+                li.className = 'compleet-item';
+                li.innerHTML = `<a href="#" data-id="${myJson[i].ID}" class="compleet-link">${myJson[i].NAME}</a>`
+                ul.appendChild(li);                                     
+            }
+            streetCompleet.classList.add('active');
+            streetCompleet.appendChild(ul);
+            choiceStreet();  
+            bodyClick();                         
+        }     
+
         let choiceStreet = () => {
             let compleetlinks = document.querySelectorAll('.compleet-link');        
             compleetlinks.forEach((link)=>{
@@ -257,12 +303,10 @@ if(orderform){
                     street.value = link.textContent;
                     hiddenStreet.value = link.dataset.id;
                     streetCompleet.classList.remove('active');
-                    streetCompleet.innerHTML = "";                            
+                    streetCompleet.innerHTML = "";   
+                    checkform(orderForm)                         
                 })
             })
-        }
-    }
+        }   
 }
-$('button.disabled').click((e)=>{
-    e.preventDefault()
-})
+
